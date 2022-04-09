b1915c5661c3b9aae33096e3e191eec4b83d9991
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-04-09 16:55:06 +0530

    Added solution - LeetHub

--- Kth smallest element - GFG/kth-smallest-element.cpp
+++ Kth smallest element - GFG/kth-smallest-element.cpp
@@ -13,10 +13,47 @@ class Solution{
     // l : starting index of the array i.e 0
     // r : ending index of the array i.e size-1
     // k : find kth smallest element and return using this function
+    int partition(int arr[] , int l , int r){
+        int j=l,x=arr[r];
+        for(int i=l;i<r;i++){
+            if(arr[i]<=x)
+            {
+                swap(arr[i] , arr[j]);
+                j++;
+            }
+        }
+        swap(arr[j] , arr[r]);
+        return j;
+    }
+    int rpartition(int arr[] , int l , int r){
+        int pivot=rand()%(r-l+1);
+        swap(arr[pivot+l], arr[r]);
+        partition(arr, l ,r);
+    }
     int kthSmallest(int arr[], int l, int r, int k) {
         //code here
-        sort(arr,arr+r+1);
-        return arr[k-1];
+        // sort(arr,arr+r+1);
+        // return arr[k-1];
+        
+        // returning kth smallest element using quicksort method since elements are distinct
+        // Time Complexity : O(N) average
+        
+        
+        // We can also do it by heap data structure
+        if(k>0 && k<=(r-l+1)){
+            
+            int pos=rpartition(arr, l, r);
+            
+            if(pos-l==(k-1))
+            return arr[pos];
+            
+            if(pos-l>(k-1)){
+                return kthSmallest(arr , l , pos-1 , k);
+            }
+            else
+            return kthSmallest(arr , pos+1 , r , k-pos+l-1);
+        }
+        
     }
 };
 
