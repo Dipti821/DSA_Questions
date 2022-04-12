05a7173efc7584728ab19872b58342e0d55ea6f2
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-04-12 10:43:09 +0530

    Added solution - LeetHub

--- Move all negative elements to end - GFG/move-all-negative-elements-to-end.cpp
+++ Move all negative elements to end - GFG/move-all-negative-elements-to-end.cpp
@@ -5,6 +5,41 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
     public:
+    void merge(int a[] ,int l ,int m , int r){
+        int n1=m-l+1;
+        int n2=r-m;
+        
+        int left[n1] ,right[n2];
+        for(int i=0;i<n1;i++)
+        left[i]=a[l+i];
+        for(int i=0;i<n2;i++)
+        right[i]=a[m+1+i];
+        
+        
+        int i=0, j=0 ,k=l;
+        
+        while(i<n1 && left[i]>0)
+        a[k++]=left[i++];
+        
+        while(j<n2 && right[j]>0)
+        a[k++]=right[j++];
+        
+        while(i<n1)
+        a[k++]=left[i++];
+        
+        while(j<n2)
+        a[k++]=right[j++];
+        
+    }
+    void RearrangeNeg(int a[] , int l ,int r){
+      if(l<r){
+          int m=l+(r-l)/2;
+          RearrangeNeg(a , l ,m);
+          RearrangeNeg(a , m+1 ,r);
+          
+          merge(a,l,m,r);
+      }  
+    }
     void segregateElements(int a[],int n)
     {
     // This solution is not optimised (modified insertion sort) TC: O(N^2) SC: O(1);
@@ -23,22 +58,27 @@ class Solution{
     //   }
     
     
-    // With auxiliary space
-    int temp[n];
-    int j=0;
-    for(int i=0;i<n;i++)
-    {
-        if(a[i]>0)
-        temp[j++]=a[i];
-    }
-    for(int i=0;i<n;i++)
-    {
-        if(a[i]<0)
-        temp[j++]=a[i];
-    }
+    // With SC: O(N) TC: O(N)
     
-    for(int i=0;i<n;i++)
-    a[i]=temp[i];
+    // int temp[n];
+    // int j=0;
+    // for(int i=0;i<n;i++)
+    // {
+    //     if(a[i]>0)
+    //     temp[j++]=a[i];
+    // }
+    // for(int i=0;i<n;i++)
+    // {
+    //     if(a[i]<0)
+    //     temp[j++]=a[i];
+    // }
+    
+    // for(int i=0;i<n;i++)
+    // a[i]=temp[i];
+    
+    //with TC: O(N) SC: O(1)
+    RearrangeNeg(a , 0, n-1);
+  
     
     }
 };
