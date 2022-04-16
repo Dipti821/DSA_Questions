989161b9ea5db2d74811dbb385e58b48eee17d52
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-04-16 14:09:48 +0530

    Time: 24 ms (42.72%), Space: 24 MB (44.69%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
@@ -20,7 +20,7 @@ public:
         if(root->val <low){
             return trimBST( root->right, low, high);
         }
-          root->left=trimBST( root->left, low, high);
+        root->left=trimBST( root->left, low, high);
         root->right=trimBST( root->right, low, high);
         
         return root;
