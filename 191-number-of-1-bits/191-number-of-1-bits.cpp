3b454680c8ef5157f4e372dc36cb94254ae5a0ae
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-05-26 13:10:22 +0530

    Time: 3 ms (54.92%), Space: 5.8 MB (96.65%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int ans= __builtin_popcount(n);
+        // int ans= __builtin_popcount(n);
+        // return ans;
+        
+        int ans=0;
+        for(int i=0;i<32;i++){
+            if(1<<i & n)
+              ans++;  
+        }
         return ans;
     }
 };
