41a604b27c2cf82acfbf37fd6aa5f52bc4b6b6ff
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-05-28 06:56:02 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (90.93%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -5,20 +5,31 @@ public:
         // Dutch National Flag Problem
         int sz=nums.size();
         int low_i=0,high_i=sz-1,mid=0;
-         while(mid<=high_i){
-            if(nums[mid]==0)
-            {
-                swap(nums[mid],nums[low_i]);
-                low_i++;
-                mid++;
+        //  while(mid<=high_i){
+        //     if(nums[mid]==0)
+        //     {
+        //         swap(nums[mid],nums[low_i]);
+        //         low_i++;
+        //         mid++;
+        //     }
+        //     else if(nums[mid]==2)
+        //     {
+        //         swap(nums[mid],nums[high_i]);
+        //         high_i--;
+        //     }
+        //     else
+        //         mid++;
+        // }
+        
+        while(mid<=high_i){
+            switch(nums[mid]){
+                case 0: swap(nums[mid++],nums[low_i++]);
+                        break;
+                case 1: mid++;
+                        break;
+                case 2:  swap(nums[mid],nums[high_i--]);
+                        break;
             }
-            else if(nums[mid]==2)
-            {
-                swap(nums[mid],nums[high_i]);
-                high_i--;
-            }
-            else
-                mid++;
         }
     }
 };
