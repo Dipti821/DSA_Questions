284b37db863891e98e60aa24377e062d8b1260dd
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-05-28 16:59:50 +0530

    Time: 107 ms (92.61%), Space: 61.2 MB (61.71%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -10,5 +10,14 @@ public:
             else
                return nums[0];
         }
+//         int sz=nums.size();
+//         for(int i=0;i<sz;i++)
+//           nums[nums[i]%sz]+=sz;
+        
+//         for(int i=0;i<sz;i++){
+//             if(nums[i]>=2*sz)
+//                 return i;
+//         }
+//         return 0;
     }
 };
