55af0d7f65a6920324c033c045b4cc2b1e7b3425
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-05-31 10:42:54 +0530

    Time: 4 ms (74.59%), Space: 9.6 MB (52.87%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -4,27 +4,27 @@ public:
         int row=matrix.size();
         int col=matrix[0].size();
         
-//         int l = 0, r = row*col - 1;
-//         while (l != r){
-//             int mid = (l + r) >> 1;
-//             if (matrix[mid /col][mid % col] < target)
-//                 l = mid + 1;
-//             else 
-//                 r = mid;
-//         }
-//         return matrix[r / col][r % col] == target;
-        
-        
-        int i=0, j=col-1;
-        while(i<row && j>=0)
-        {
-            if(matrix[i][j]==target)
-                return true;
-            else if(matrix[i][j]>target)
-                j--;
-            else
-                i++;
+        int l = 0, r = row*col - 1;
+        while (l != r){
+            int mid = (l + r) >> 1;
+            if (matrix[mid /col][mid % col] < target)
+                l = mid + 1;
+            else 
+                r = mid;
         }
-        return false;
+        return matrix[r / col][r % col] == target;
+        
+        
+        // int i=0, j=col-1;
+        // while(i<row && j>=0)
+        // {
+        //     if(matrix[i][j]==target)
+        //         return true;
+        //     else if(matrix[i][j]>target)
+        //         j--;
+        //     else
+        //         i++;
+        // }
+        // return false;
     }
 };
