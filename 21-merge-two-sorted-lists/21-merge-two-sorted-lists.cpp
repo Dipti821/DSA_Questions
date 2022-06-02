5ba2994bc663c6a15ba2bf1850b13b07937739b9
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-06-02 10:46:46 +0530

    Time: 7 ms (83.45%), Space: 14.8 MB (44.02%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,42 +11,68 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* ans=new ListNode(0);
-        ListNode* temp=ans;
-        while(list1 !=NULL || list2!=NULL){
-            if(list1==NULL){
-                while(list2!=NULL){
-                    temp->next=list2;
-                    temp=temp->next;
-                    list2=list2->next;
-                }
-                break;
+        
+        // Using external linked list
+        
+        // ListNode* ans=new ListNode(0);
+        // ListNode* temp=ans;
+        // while(list1 !=NULL || list2!=NULL){
+        //     if(list1==NULL){
+        //         while(list2!=NULL){
+        //             temp->next=list2;
+        //             temp=temp->next;
+        //             list2=list2->next;
+        //         }
+        //         break;
+        //     }
+        //     if(list2==NULL){
+        //         while(list1!=NULL){
+        //             temp->next=list1;
+        //             temp=temp->next;
+        //             list1=list1->next;
+        //         }
+        //         break;
+        //     }
+        //     if(list1->val>=list2->val){
+        //         temp->next=list2;
+        //         temp=temp->next;
+        //         if(list2->next==NULL)
+        //             list2=NULL;
+        //         else
+        //         list2=list2->next;
+        //     }
+        //     else {
+        //         temp->next=list1;
+        //         temp=temp->next;
+        //          if(list1->next==NULL)
+        //             list1=NULL;
+        //         else
+        //         list1=list1->next;
+        //     }
+        // }
+        // return ans->next;
+        
+        // in place method
+        // TC: O(N1+N2) SC: O(1) 
+        
+        if(list1==NULL) return list2;
+        if(list2==NULL) return list1;
+        
+        ListNode *temp=NULL,*res;
+        if(list1->val>list2->val)
+            swap(list1,list2);
+        res=list1;
+        while(list1!=NULL && list2!=NULL){
+            temp=NULL;
+            while(list1!=NULL && list1->val<=list2->val){
+               temp=list1;
+               list1=list1->next;
+                
             }
-            if(list2==NULL){
-                while(list1!=NULL){
-                    temp->next=list1;
-                    temp=temp->next;
-                    list1=list1->next;
-                }
-                break;
-            }
-            if(list1->val>=list2->val){
-                temp->next=list2;
-                temp=temp->next;
-                if(list2->next==NULL)
-                    list2=NULL;
-                else
-                list2=list2->next;
-            }
-            else {
-                temp->next=list1;
-                temp=temp->next;
-                 if(list1->next==NULL)
-                    list1=NULL;
-                else
-                list1=list1->next;
-            }
+            temp->next=list2;
+            swap(list1,list2);
+            
         }
-        return ans->next;
+       return res; 
     }
 };
