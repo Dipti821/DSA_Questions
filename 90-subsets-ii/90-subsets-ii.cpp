c61cf657631391134be25b3df5989a91b588640a
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-06-04 11:15:34 +0530

    Time: 4 ms (66.96%), Space: 7.6 MB (73.35%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void generateSubset(int ind ,vector<int>&subset,vector<int>&nums,vector<vector<int>>&res){
+         res.push_back(subset);
+         for (int i = ind; i < nums.size(); i++) {
+            if (i != ind && nums[i] == nums[i - 1]) continue;
+            subset.push_back(nums[i]);
+            generateSubset(i + 1,subset, nums,res);
+            subset.pop_back();
+         }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        // Brute force : Method 1: use pick and not pick technique to generate all subsets 
+        // store it in set to ignore duplicates
+        
+        // Method 2:
+        vector<vector<int>>res;
+        vector<int>subset;
+        sort(nums.begin(),nums.end());
+        generateSubset(0,subset,nums,res);
+        return res;
+    }
+};
