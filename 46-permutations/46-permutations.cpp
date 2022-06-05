4dc3eb0f26430e7fd177226ed309d546666928fa
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-06-05 19:25:11 +0530

    Time: 9 ms (19.71%), Space: 8.2 MB (21.19%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,28 +1,49 @@
 class Solution {
 public:
-    void cal(vector<int>&nums,vector<int>&permu,vector<vector<int>>&ans,int freq[])    {
-       if(permu.size()==nums.size()){
+    // void cal(vector<int>&nums,vector<int>&permu,vector<vector<int>>&ans,int freq[])    {
+    //    if(permu.size()==nums.size()){
+    //        ans.push_back(permu);
+    //        return;
+    //    }
+    //     for(int i=0;i<nums.size();i++){
+    //         if(freq[i]==0){
+    //         permu.push_back(nums[i]);
+    //         freq[i]=1;
+    //         cal(nums,permu,ans,freq);
+    //         permu.pop_back();
+    //         freq[i]=0;
+    //         }
+    //     }
+    // }
+    // vector<vector<int>> permute(vector<int>& nums) {
+    //     //TC: N!*N SC: O(N)+O(N)
+    //     vector<vector<int>>ans;
+    //     vector<int>permu;
+    //     int n=nums.size();
+    //     int freq[nums.size()]; 
+    //     for(int i = 0;i<nums.size();i++) freq[i] = 0; 
+    //     cal(nums,permu,ans,freq);
+    //     return ans;
+    // }
+    
+    // Method 2: SC: O(N) TC: O(N*N!)
+      void cal(int ind, vector<int>&nums,vector<vector<int>>&ans) {
+       if(ind==nums.size()){
+           vector<int>permu;
+           for(int i=0;i<nums.size();i++)
+               permu.push_back(nums[i]);
            ans.push_back(permu);
            return;
        }
-        for(int i=0;i<nums.size();i++){
-            if(freq[i]==0){
-            permu.push_back(nums[i]);
-            freq[i]=1;
-            cal(nums,permu,ans,freq);
-            permu.pop_back();
-            freq[i]=0;
-            }
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[ind],nums[i]);
+            cal(ind+1,nums,ans);
+            swap(nums[ind],nums[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        //TC: N!*N
         vector<vector<int>>ans;
-        vector<int>permu;
-        int n=nums.size();
-        int freq[nums.size()]; 
-        for(int i = 0;i<nums.size();i++) freq[i] = 0; 
-        cal(nums,permu,ans,freq);
+        cal(0,nums,ans);
         return ans;
     }
 };
