f082d73911ca1d7665530ba2ea29b79cf7a4afad
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-06-10 11:09:10 +0530

    Time: 0 ms (100.00%), Space: 7 MB (17.24%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -6,12 +6,21 @@ public:
     }
     
     void push(int x) {
-        q2.push(x);
-        while(!q1.empty()){
-            q2.push(q1.front());
+        // q2.push(x);
+        // while(!q1.empty()){
+        //     q2.push(q1.front());
+        //     q1.pop();
+        // }
+        // swap(q1,q2);
+        
+        // using only one queue
+        q1.push(x);
+        int k=q1.size();
+        k--;
+        while(k--){
+            q1.push(q1.front());
             q1.pop();
         }
-        swap(q1,q2);
     }
     
     int pop() {
