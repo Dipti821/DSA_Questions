d0715508eefdb912e2eb2f3e4ad5dfb3a73da466
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-06-12 14:43:32 +0530

    Time: 582 ms (21.02%), Space: 134.7 MB (53.29%) - LeetHub

---
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        // TC: O(N)+O(N)  SC: O(K)
+       deque<int>dq;
+       vector<int>ans;
+       for(int i = 0; i < nums.size(); i++) {
+          if (!dq.empty() && dq.front()==i - k) dq.pop_front();//removing out of bound index
+
+          while (!dq.empty() && nums[dq.back()] < nums[i])
+            dq.pop_back();
+
+          dq.push_back(i);
+          if (i >= k - 1) ans.push_back(nums[dq.front()]);
+        }
+  return ans; 
+    }
+};
