dbe29f42a6dac8dce7153598f99fc21e5270c8fa
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-06-12 14:44:02 +0530

    Time: 581 ms (21.12%), Space: 134.7 MB (43.78%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -7,7 +7,7 @@ public:
        for(int i = 0; i < nums.size(); i++) {
           if (!dq.empty() && dq.front()==i - k) dq.pop_front();//removing out of bound index
 
-          while (!dq.empty() && nums[dq.back()] < nums[i])
+          while (!dq.empty() && nums[dq.back()] <= nums[i])
             dq.pop_back();
 
           dq.push_back(i);
