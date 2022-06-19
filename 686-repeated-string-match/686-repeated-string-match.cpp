1b12433bc5b54e26c55165abfa24d95d48dba365
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-06-19 18:23:59 +0530

    Time: 3 ms (97.47%), Space: 7.5 MB (29.79%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.cpp
+++ 686-repeated-string-match/686-repeated-string-match.cpp
@@ -1,7 +1,17 @@
 class Solution {
+    // public:
+    // int repeatedStringMatch(string a, string b) {
+    //      for (auto i = 0, j = 0; i < a.size(); ++i) {
+    //         for (j = 0; j < b.size() && a[(i + j) % a.size()] == b[j]; ++j);
+    //         if (j == b.size()) 
+    //         return (i + j - 1) / a.size() + 1;
+    // }
+    // return -1;
+    // }
 private:
     int BASE = 1000000;
 public:
+    // Rabin Karp Algorithm
     int repeatedStringMatch(string A, string B) {
         if(A == B) return 1;
         int count = 1;
