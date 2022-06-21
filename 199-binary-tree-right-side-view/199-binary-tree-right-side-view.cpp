32e639805f379b0cf92345ce749aac9c68e1ddf0
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-06-21 13:29:20 +0530

    Time: 7 ms (43.88%), Space: 11.9 MB (66.85%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -11,30 +11,43 @@
  */
 class Solution {
 public:
+    void recursion(TreeNode *root, int level, vector<int> &res)
+    {
+        if(root==NULL) return ;
+        if(res.size()==level) res.push_back(root->val);
+        recursion(root->right, level+1, res);
+        recursion(root->left, level+1, res);
+    }
+    
     vector<int> rightSideView(TreeNode* root) {
-        vector<int>ans;
+        // vector<int>ans;
+        // if(root == NULL) return ans;
         
-        if(root == NULL) return ans;
+        // TC: O(N) ,SC: O(N) ITERATIVE SOLN
+//         queue<pair<TreeNode* ,int>>q;
+//         q.push({root ,0});
+//         map<int,int>m;
         
-        queue<pair<TreeNode* ,int>>q;
-        q.push({root ,0});
-        map<int,int>m;
-        
-        while(!q.empty()){
-            auto it=q.front();
-            q.pop();
+//         while(!q.empty()){
+//             auto it=q.front();
+//             q.pop();
             
-            m[it.second]=it.first->val;
-            if(it.first->left)
-                q.push({it.first->left ,it.second+1});
-            if(it.first->right)
-                q.push({it.first->right ,it.second+1});
+//             m[it.second]=it.first->val;
+//             if(it.first->left)
+//                 q.push({it.first->left ,it.second+1});
+//             if(it.first->right)
+//                 q.push({it.first->right ,it.second+1});
             
-        }
-        for(auto i:m)
-            ans.push_back(i.second);
+//         }
+//         for(auto i:m)
+//             ans.push_back(i.second);
         
-        return ans;
-            
+//         return ans;
+        
+        // Recursive solution TC: O(N) ,SC: O(H)
+        
+        vector<int> res;
+        recursion(root, 0, res);
+        return res;
     }
 };
