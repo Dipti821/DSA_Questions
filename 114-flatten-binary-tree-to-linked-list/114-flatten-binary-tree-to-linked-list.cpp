b59b55da44bfc7157bb1e990a8f8f6f5d6558b6d
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-06-24 16:46:38 +0530

    Time: 8 ms (64.24%), Space: 12.7 MB (78.53%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -24,23 +24,41 @@ public:
 //       prev = root; 
         
         // using stack TC: O(N) SC: O(N)
-        if (root == NULL) return;
-  stack < TreeNode * > st;
-  st.push(root);
-  while (!st.empty()) {
-    TreeNode * cur = st.top();
-    st.pop();
+//         if (root == NULL) return;
+//   stack < TreeNode * > st;
+//   st.push(root);
+//   while (!st.empty()) {
+//     TreeNode * cur = st.top();
+//     st.pop();
 
-    if (cur -> right != NULL) {
-      st.push(cur -> right);
-    }
-    if (cur -> left != NULL) {
-      st.push(cur -> left);
-    }
-    if (!st.empty()) {
-      cur -> right = st.top();
-    }
-    cur -> left = NULL;
-  }
+//     if (cur -> right != NULL) {
+//       st.push(cur -> right);
+//     }
+//     if (cur -> left != NULL) {
+//       st.push(cur -> left);
+//     }
+//     if (!st.empty()) {
+//       cur -> right = st.top();
+//     }
+//     cur -> left = NULL;
+//   }
+        
+        //TC: O(N)  SC:O(1)
+          TreeNode* cur = root;
+		while (cur)
+		{
+			if(cur->left)
+			{
+				TreeNode* pre = cur->left;
+				while(pre->right)
+				{
+					pre = pre->right;
+				}
+				pre->right = cur->right;
+				cur->right = cur->left;
+				cur->left = NULL;
+			}
+			cur = cur->right;
+		}
     }
 };
