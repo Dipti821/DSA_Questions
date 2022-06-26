8953d08349c54eb975c7cca8a9084145d131176c
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-06-26 15:02:30 +0530

    Time: 36 ms (27.12%), Space: 16.7 MB (96.62%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -19,33 +19,48 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
-        if(!root) return NULL;
+//         if(!root) return NULL;
         
-        queue<Node*>q;
-        q.push(root);
+//         queue<Node*>q;
+//         q.push(root);
         
-        while(!q.empty()){
-            int sz=q.size();
+//         while(!q.empty()){
+//             int sz=q.size();
             
-                Node* temp=q.front();
-                q.pop();
-            if(temp->left)
-                q.push(temp->left);
-            if(temp->right)
-                q.push(temp->right);
+//                 Node* temp=q.front();
+//                 q.pop();
+//             if(temp->left)
+//                 q.push(temp->left);
+//             if(temp->right)
+//                 q.push(temp->right);
      
-            for(int i=1;i<sz;i++){
-                temp->next=q.front();
-                temp=q.front();
-                 if(temp->left)
-                q.push(temp->left);
-            if(temp->right)
-                q.push(temp->right);
-                q.pop();
-            }
-            temp->next=NULL;
+//             for(int i=1;i<sz;i++){
+//                 temp->next=q.front();
+//                 temp=q.front();
+//                  if(temp->left)
+//                 q.push(temp->left);
+//             if(temp->right)
+//                 q.push(temp->right);
+//                 q.pop();
+//             }
+//             temp->next=NULL;
             
+//         }
+//         return root;
+        
+          if (root == NULL) return NULL;
+    Node *pre = root;
+    Node *cur = NULL;
+    while(pre->left) {
+        cur = pre;
+        while(cur) {
+            cur->left->next = cur->right;
+            if(cur->next) cur->right->next = cur->next->left;
+            cur = cur->next;
         }
+        pre = pre->left;
+    }
         return root;
     }
+    
 };
