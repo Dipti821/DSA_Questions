0acec105553c229a2c592e59f07c350d0896c11a
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-06-27 21:52:26 +0530

    Added solution - LeetHub

--- Height of Binary Tree - GFG/height-of-binary-tree.cpp
+++ Height of Binary Tree - GFG/height-of-binary-tree.cpp
@@ -100,14 +100,13 @@ class Solution{
     public:
     //Function to find the height of a binary tree.
     int height(struct Node* node){
-         //code here
-    if(node==NULL)
-    return 0;
-    
-    int lh=height(node->left);
-    int rh=height(node->right);
-    
-    return 1+max(lh,rh);
+        // code here 
+         if(node== NULL) return 0;
+   
+   int lh=height(node->left);
+   int rh=height(node->right);
+   
+   return 1+max(lh,rh);
     }
 };
 
