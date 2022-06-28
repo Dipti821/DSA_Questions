3044ed3d13458e19515e04bab5623e8cfcd3ceb9
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-06-28 12:58:24 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -8,42 +8,84 @@ using namespace std;
 class Solution
 {
 	public:
+	
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        // code here
-        vector<int>key(V,INT_MAX);
-        vector<bool>mst(V,false);
-        key[0]=0;
-        for(int count=0;count<V-1;count++)
-        {
-            int mini=INT_MAX , u;
-            for(int v=0; v<V;v++)
-            {
-                if(mst[v]==false && key[v]<mini)
-                {
-                    mini=key[v];
-                    u=v;
-                }
-            }
-            mst[u]=true;
-            for(auto it : adj[u])
-            {
-               int k=it[0];
-               int weight=it[1];
-               if(mst[k]==false && weight<key[k])
-               {
-                   key[k]=weight;
-               }
-            }
-        }
-        int sum=0;
-        for(int i=0;i<V;i++)
-        {
-            sum+=key[i];
-        }
-        return sum;
+        // Prims Algo
         
+        // code here
+        // vector<int>key(V,INT_MAX);
+        // vector<bool>mst(V,false);
+        // key[0]=0;
+        // for(int count=0;count<V-1;count++)
+        // {
+        //     int mini=INT_MAX , u;
+        //     for(int v=0; v<V;v++)
+        //     {
+        //         if(mst[v]==false && key[v]<mini)
+        //         {
+        //             mini=key[v];
+        //             u=v;
+        //         }
+        //     }
+        //     mst[u]=true;
+        //     for(auto it : adj[u])
+        //     {
+        //       int k=it[0];
+        //       int weight=it[1];
+        //       if(mst[k]==false && weight<key[k])
+        //       {
+        //           key[k]=weight;
+        //       }
+        //     }
+        // }
+        // int sum=0;
+        // for(int i=0;i<V;i++)
+        // {
+        //     sum+=key[i];
+        // }
+        // return sum;
+        
+        //Kruskal's Algo
+        
+         vector<int> parent(V), rank(V);
+        for(int x = 0; x < V; x++) {
+            parent[x] = x;
+            rank[x] = 1;
+        }
+        
+        auto find = [&](int node, auto &find) -> int {
+            return (parent[node] == node ? node : parent[node] = find(parent[node], find));
+        };
+        
+        auto unite = [&](int node1, int node2) -> void {
+            node1 = find(node1, find);
+            node2 = find(node2, find);
+            if(node1 != node2){
+                if(rank[node1] < rank[node2]){
+                    swap(node1, node2);
+                }
+                parent[node2] = node1;
+                rank[node1] += rank[node2];
+            }
+        };
+        
+        int ans = 0;
+        vector<pair<int, pair<int, int>>> vp;
+        for(int x = 0; x < V; x++){
+            for(auto z: adj[x]){
+                vp.push_back({z[1], {x, z[0]}});
+            }
+        }
+        sort(vp.begin(), vp.end());
+        for(auto x: vp){
+            if(find(x.second.first, find) != find(x.second.second, find)) {
+                ans+=x.first;
+                unite(x.second.first, x.second.second);
+            }
+        }
+        return ans;
     }
 };
 
