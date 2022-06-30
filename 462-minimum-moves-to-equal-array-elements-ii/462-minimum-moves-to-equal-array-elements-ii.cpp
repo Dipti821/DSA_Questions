f0d1bf92bf9c0a8a1f7b33584cf2ce2e0747e10e
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-06-30 11:32:09 +0530

    Time: 27 ms (9.46%), Space: 10.8 MB (59.74%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -4,15 +4,24 @@ public:
         int median=0;
         sort(nums.begin(),nums.end());
         
-        if(nums.size()&1)
-            median=nums[nums.size()/2];
-        else
-            median=(nums[nums.size()/2]+nums[(nums.size()/2)-1])/2;
+//         if(nums.size()&1)
+//             median=nums[nums.size()/2];
+//         else
+//             median=(nums[nums.size()/2]+nums[(nums.size()/2)-1])/2;
+        
+//         int ans=0;
+//         for(int i=0;i<nums.size();i++)
+//             ans+=abs(median-nums[i]);
+        
+//         return ans;
         
         int ans=0;
-        for(int i=0;i<nums.size();i++)
-            ans+=abs(median-nums[i]);
-        
+        int i=0,j=nums.size()-1;
+        while(i<j){
+            ans+=(nums[j]-nums[i]);
+            i++,j--;
+        }
         return ans;
+            
     }
 };
