b803398c16b194a7e7b2e66f7b8e2c1fb5996182
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-07-28 21:09:04 +0530

    Time: 20 ms (32.51%), Space: 7.2 MB (77.97%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()) return false;
-        
         unordered_map<char,int>m;
-        for(int i=0;i<s.length();i++){
-            m[s[i]]++;
-            m[t[i]]--;
+        for(auto c: s)
+            m[c]++;
+        for(auto c: t){
+            if(m.find(c)==m.end())
+                return false;
+            m[c]--;
+            if(m[c]==0)
+                m.erase(c);
         }
-        for(auto it: m)
-            if(it.second) return false;
-        
+        if(m.empty())
         return true;
+        else
+        return false;
     }
 };
