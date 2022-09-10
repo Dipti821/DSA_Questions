6634b0a7e1a063fd47eb95f0ad4e8a937b7d38a9
Dipti Agarwal <84309022+Dipti821@users.noreply.github.com>
2022-09-10 22:16:16 +0530

    Time: 12 ms (75.26%), Space: 10.7 MB (98.77%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -11,18 +11,16 @@ public:
 
 int maxProfit(int k, vector<int> &prices) {
     if(k == 0 || prices.empty()) return 0;
-    // Max profit by k transaction from 0 to i
+    
     const int n = prices.size();        
     if(k >= n / 2) return maxProfitInf(prices);
 
-    // balance - the balance with at most j transactions with item 0 to i
-    // profit - the profit with at most j transactions with item 0 to i
     vector<int> balance(k + 1, INT_MIN), profit(k + 1);
     
     for(int i = 0; i < n; ++i) {
         for(int j = 1; j <= k; ++j) {
-            balance[j] = max(profit[j - 1] - prices[i], balance[j]); // whether to buy at prices[i]
-            profit[j] = max(balance[j] + prices[i], profit[j]); // whether to sell at prices[i]
+            balance[j] = max(profit[j - 1] - prices[i], balance[j]); 
+            profit[j] = max(balance[j] + prices[i], profit[j]); 
         }
     }
     
